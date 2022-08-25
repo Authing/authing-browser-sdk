c65f1f35cd4d6495282b3e34bc3593a638f2f9a3
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2022-08-25 19:49:32 +0800

    chore: update example

--- example/sso/vue2/website1/src/App.vue
+++ example/sso/vue2/website1/src/App.vue
@@ -54,13 +54,13 @@ export default {
     this.sdk = new Authing({
       // 很重要，请仔细填写！
       // 如果应用开启 SSO，这儿就要写单点登录的“应用面板地址”；否则填写应用的“认证地址”。
-      domain: "应用面板地址",
+      domain: "https://mlbkhepjgjiihaap.authing.cn",
 
       // 应用 ID
-      appId: "应用 ID",
+      appId: "62e752f0d8c681db4ed3f743",
 
       // 登录回调地址，需要在控制台『应用配置 - 登录回调 URL』中指定
-      redirectUri: "登录回调地址",
+      redirectUri: "https://localhost:8000",
     });
   },
   mounted() {
@@ -100,6 +100,12 @@ export default {
     async getLoginState() {
       const state = await this.sdk.getLoginState();
       this.loginState = state;
+      console.log(this.loginState);
+
+      const userInfo = await this.sdk.getUserInfo({
+        accessToken: state.accessToken
+      });
+      console.log('userInfo: ', userInfo);
     },
     /**
      * 用 Access Token 获取用户身份信息
